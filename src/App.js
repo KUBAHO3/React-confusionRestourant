601483b7f614f942acebaa189d05f90b27828f07
KUBAHO3 <kubaholinne@gmail.com>
2022-01-14 15:16:06 +0530

    List of items are added to our confusion reastourant

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponents';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Navbar dark color="secondary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
     </div>
   );
 }
