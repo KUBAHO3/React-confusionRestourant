b95b82db13670667e45177f2c2326486eb963f18
KUBAHO3 <kubaholinne@gmail.com>
2022-01-22 15:00:08 +0530

    After week 1 fourth commit with dish detail

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,65 +1,48 @@
-import React, {Component} from 'react';
-import {Media} from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
-
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
+// this is how I add new component to the react app
 class Menu extends Component {
+
     constructor(props) {
         super(props);
-        this.state = { 
-              selectedDish: null
-        }
-         }
-         onDishSelect(dish) {
-          this.setState({ selectedDish: dish});
-      }
-  
-      renderDish(dish) {
-          if (dish != null)
-              return(
-                  <Card>
-                      <CardImg top src={dish.image} alt={dish.name} />
-                      <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                      </CardBody>
-                  </Card>
-              );
-          else
-              return(
-                  <div></div>
-              );
-      }
-  
 
-      render() {
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({selectedDish: dish})
+    }
+
+
+
+    // every component should implement this method render
+    render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
+                // col 12 for extra-small and small screens
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
             );
         });
 
         return (
-          <div className="container">
-              <div className="row">
-                  {menu}
-              </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
+            <div className="container">
+                <div className="row">
+                    {menu}
                 </div>
-              </div>
-          </div>
-      );
-  }
+                <DishDetail dish={this.state.selectedDish} />
+            </div>
+        );
+    }
 }
-
- 
+// always export the component to use it somewhere else
 export default Menu;
